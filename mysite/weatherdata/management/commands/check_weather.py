e37a2ea92162c0cad36ffae27ac647697fd96a4b
Dodiadevyani <devd0093@gmail.com>
2016-12-08 16:06:33 +0530

    Update check_weather.py

--- mysite/weatherdata/management/commands/check_weather.py
+++ mysite/weatherdata/management/commands/check_weather.py
@@ -1,1 +1,22 @@
+import httplib
+
+parameters = {"Tmax","Tmin","Tmean","Sunshine","Rainfall"}
+locations = {"UK","England","Wales","Scotland"}
+count = 0;
+for l in locations:
+	for p in parameters:
+		connection = httplib.HTTPConnection("www.metoffice.gov.uk")
+		url = "/pub/data/weather/uk/climate/datasets/"+p+"/date/"+l+".txt"
+		print url
+		connection.request("GET", url)
+		r1 = connection.getresponse()
+		#print r1.status, r1.reason
+		weather_data = r1.read()
+		weather_filename = p+"_"+l+".txt"
+		weather_file = open(weather_filename, 'w')
+		weather_file.truncate()
+		weather_file.write(weather_data)
+		weather_file.close()
+		connection.close()
+
 
