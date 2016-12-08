cf253b255588e851246460c8f5997339568506ee
Dodiadevyani <devd0093@gmail.com>
2016-12-08 15:50:08 +0530

    Update models.py

--- mysite/weatherdata/models.py
+++ mysite/weatherdata/models.py
@@ -24,3 +24,24 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class WeatherLog(models.Model):
+    location = models.ForeignKey(Location, verbose_name=_("location"))
+    timestamp = models.DateTimeField(_("timestamp"))
+    mean_temperature = models.IntegerField(_("temperature (C°)"))
+    rainfall = models.IntegerField(_("rainfall (mm)"))
+    min_temperature = models.IntegerField(_("temperature (C°)"))
+    max_temperature = models.IntegerField(_("temperature (C°)"))
+    sunshine = models.IntegerField(_("sunshine (hrs)"))
+    
+    class Meta:
+        verbose_name=_("weather log")
+        verbose_name_plural=_("weather logs")
+        ordering = ("-timestamp",)
+    
+    def __unicode__(self):
+        return "%s @ %s" % (
+            self.location.name,
+            self.timestamp.strftime("%Y-%m-%dT%H:%M"),
+            )
+
