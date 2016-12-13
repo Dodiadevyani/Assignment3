b20c1450b9fefea39f5b49c372fc5af9d4ffe7d7
Dodiadevyani <devd0093@gmail.com>
2016-12-13 08:45:07 +0530

    Update views.py

--- mysite/weatherdata/views.py
+++ mysite/weatherdata/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Post, Weather
+
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the weatherdata index.")
-
+    template = loader.get_template('weatherdata/template.html')
+    context={
+        'obj': Post.objects.all(),
+        'sub': Weather.objects.all(),
+    }    
+    return HttpResponse(template.render(context,request))
