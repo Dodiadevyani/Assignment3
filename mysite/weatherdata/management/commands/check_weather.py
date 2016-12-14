c6a32009446725b0fd26eb1b85f5ba8a167a7c43
Dodiadevyani <devd0093@gmail.com>
2016-12-14 08:42:27 +0530

    Update check_weather.py

--- mysite/weatherdata/management/commands/check_weather.py
+++ mysite/weatherdata/management/commands/check_weather.py
@@ -9,43 +9,26 @@ class Command(BaseCommand):
                 locations = {"UK","England","Wales","Scotland"}
                 count = 0;
                 for l in locations:
-                    print l
-                    #for p in parameters:
-                    connection = httplib.HTTPConnection("www.metoffice.gov.uk")
-		    url = "/pub/data/weather/uk/climate/datasets/"+"Tmax"+"/date/"+l+".txt"
-	            #print url
-		    connection.request("GET", url)
-		    r1 = connection.getresponse()
-	            #print r1.status, r1.reason
-         	    max_temp = r1.read()
-		    url = "/pub/data/weather/uk/climate/datasets/"+"Tmin"+"/date/"+l+".txt"
-	            #print url
-		    connection.request("GET", url)
-		    r1 = connection.getresponse()
-	            #print r1.status, r1.reason
-         	    min_temp = r1.read()
-         	    url = "/pub/data/weather/uk/climate/datasets/"+"Tmean"+"/date/"+l+".txt"
-	            #print url
-		    connection.request("GET", url)
-		    r1 = connection.getresponse()
-	            #print r1.status, r1.reason
-         	    mean_temp = r1.read()
-         	    url = "/pub/data/weather/uk/climate/datasets/"+"Sunshine"+"/date/"+l+".txt"
-	            #print url
-		    connection.request("GET", url)
-		    r1 = connection.getresponse()
-	            #print r1.status, r1.reason
-         	    sunshine = r1.read()
-         	    url = "/pub/data/weather/uk/climate/datasets/"+"Rainfall"+"/date/"+l+".txt"
-	            #print url
-		    connection.request("GET", url)
-		    r1 = connection.getresponse()
-	            #print r1.status, r1.reason
-         	    rainfall = r1.read()
-         	    connection.close()
-		    max_temperature=max_temp
-                    min_temperature=min_temp
-                    mean_temperature=mean_temp
-                    rainfall=rain_fall
-                    sunshine=sunshine_hrs
-		    print Post.objects.filter(location= l)
+                    print 
+                    for p in parameters:
+                        connection = httplib.HTTPConnection("www.metoffice.gov.uk")
+		        url = "/pub/data/weather/uk/climate/datasets/"+"Tmax"+"/date/"+l+".txt"
+	                #print url
+		        connection.request("GET", url)
+		        r1 = connection.getresponse()
+	                #print r1.status, r1.reason
+         	        weather_data = r1.read()
+		        #print weather_data
+	                weather_filename = p+"_"+l+".txt"
+		        weather_file = open(weather_filename, 'w')
+		        weather_file.truncate()
+		        weather_file.write(weather_data)
+		        #weather_file.close()
+         	        connection.close()
+                        print p 
+		    q= Post.objects.filter(location= l)
+         	    print q
+         	    with open ('Rainfall_Wales.txt','r') as f:
+                            for line in f:
+                                    sections=line.split('|')
+                                    Weather.objects.create(rainfall= sections)
